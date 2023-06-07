31a7a45654621a20d9b95a22f7e1d8441cf4912f
Emanuel Júnior <117325229+EmanuelJnr@users.noreply.github.com>
2023-06-07 16:45:14 -0300

    Junção order

---
+++ ecommerce/src/main/java/br/com/compassuol/sp/challenge/ecommerce/services/OrderService.java
@@ -0,0 +1,26 @@
+package br.com.compassuol.sp.challenge.ecommerce.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Order getOrderById(Long id) {
+        return orderRepository.findById(id).orElse(null);
+    }
+
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+}
