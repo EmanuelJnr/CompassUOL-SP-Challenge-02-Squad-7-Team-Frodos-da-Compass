a97ba77af9bb972f24fa733edf100c18fa0c591e
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2023-06-10 20:56:31 -0300

    Add files via upload

---
+++ ecommerce/src/main/java/br/com/compassuol/sp/challenge/ecommerce/entitys/Product.java
@@ -0,0 +1,23 @@
+package br.com.compassuol.sp.challenge.ecommerce.entity;
+
+public class Product {
+
+	private int id;
+
+	public Product() {
+
+	}
+
+	public Product(int id) {
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
