a27883039b5a113805d6af81f8d8ac99624b9b6b
Emanuel Júnior <117325229+EmanuelJnr@users.noreply.github.com>
2023-06-12 17:52:06 -0300

    Merge

--- ecommerce/src/main/resources/static/db/Ecommerce.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE ecommerce;
-
-use ecommerce;
-
-CREATE TABLE tb_products(
-productId int auto_increment not null primary key,
-name varchar(30) not null,
-price double not null,
-description varchar(255) not null);
